12c46f3b56bfca53c79a6a4eb0d95e838d040463
Ayush Garg <gargayush341@gmail.com>
2026-01-30 20:05:12 +0530

    if no data, then print summary

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -9,6 +9,9 @@ def packet_handler(pkt):
     Sniff the network packet, load the byte and decode it.
     Append it into chars array to see the data.
     """
+
+    # Data might not be there, so checking summary of the packet
+    print(pkt.summary())
     if pkt.haslayer(Raw):
         flag_byte = pkt[Raw].load
         chars.append(flag_byte.decode("utf-8"))
