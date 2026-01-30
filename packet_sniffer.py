b53f4803225148dadabecfd539026c32b755c910
Ayush Garg <gargayush341@gmail.com>
2026-01-30 17:39:16 +0530

    scapy packet sniffer

---
+++ packet_sniffer.py
@@ -0,0 +1,18 @@
+from scapy.all import sniff, Raw
+
+
+def packet_handler(pkt):
+    """
+    Sniff the network packet, load the byte and decode it.
+    Append it into chars array to see the data.
+    """
+    chars = []
+    if pkt.haslayer(Raw):
+        flag_byte = pkt[Raw].load
+        chars.append(flag_byte.decode("utf-8"))
+        if len(chars) == 150:
+            print(chars)
+            return
+
+
+sniff(filter="host 127.0.0.1 and tcp port 80", prn=packet_handler, store=False)
