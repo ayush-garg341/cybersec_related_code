acb2596d4a98b122f6820e8c58f80ce8d2c5fd63
Ayush Garg <gargayush341@gmail.com>
2026-02-04 18:40:42 +0530

    arp broadcast and waiting for reply

---
+++ arp_broadcast.py
@@ -0,0 +1,21 @@
+from scapy.all import Ether, ARP, sendp, srp
+
+iface = "eth0"
+target_ip = "10.0.0.2"
+
+pkt = Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(  # Ethernet broadcast
+    op=1, pdst=target_ip
+)  # ARP who-has
+
+sendp(pkt, iface=iface, verbose=True)
+
+# Ethernet broadcast goes out.
+# The host that owns 10.0.0.2 replies with its MAC.
+
+# Send ARP and wait for the reply (most useful)
+
+pkt = Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=target_ip)
+ans, _ = srp(pkt, iface=iface, timeout=2, verbose=False)
+
+for _, reply in ans:
+    print("IP:", reply.psrc, "MAC:", reply.hwsrc)
