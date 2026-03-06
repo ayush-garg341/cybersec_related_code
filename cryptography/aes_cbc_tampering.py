ba0981a29ed06726a733cb5b81a7b0b99b0c0be6
Ayush Garg <gargayush341@gmail.com>
2026-03-06 15:42:48 +0530

    cbc tampering

---
+++ cryptography/aes_cbc_tampering.py
@@ -0,0 +1,27 @@
+"""
+P1 = D(C1) XOR IV
+orig = b"sleep"
+target = b"flag"
+
+P1' = D(C1) XOR IV'
+but P1  = D(C1) XOR IV
+IV' = P1 XOR P1' XOR IV
+"""
+
+task = "TASK: 117797cc611b5c625819068222df0b17b780d347a89cb922c8b804b8dbfe7d71"
+
+if task.startswith("TASK: "):
+    data = bytes.fromhex(task.split()[1])
+    iv = bytearray(data[:16])
+    ciphertext = data[16:]
+    print(ciphertext)
+
+    orig = b"sleep"
+    target = b"flag!"
+
+    for i in range(len(orig)):
+        iv[i] ^= orig[i] ^ target[i]
+
+    tampered = bytes(iv) + ciphertext
+
+    print(tampered.hex())
